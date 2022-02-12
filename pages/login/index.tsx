f421887b18d30dc8857f6c9911df138774f1c3e7
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-02-13 01:06:55 +0530

    refactored code and changed Ui

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import classes from "../../styles/login.module.scss";
+import Button from '@mui/material/Button';
 const index = () => {
   return (
-      <div className={`${classes.login} flex`}>
+      <div className={`${classes.login}`}>
           <h2>Login</h2>
-      <div className={`${classes.dis} flex`}>
+     <div className="flex">
+     <div className={`${classes.dis} flex`}>
         <form className={classes.form}>
           <div className={classes.fr}>
-          <label htmlFor="outlined-search">E-MAIL</label>
+          <label htmlFor="outlined-search">E-Mail</label>
           <TextField
             id="outlined-search"
             label=""
-            type="search"
+            type="email"
             variant="outlined"
             className={classes.text}
             required
           />
+          <label htmlFor="outlined-search">password</label>
+          <TextField
+            id="outlined-search"
+            label=""
+            type="password"
+            variant="outlined"
+            className={classes.text}
+            required
+          />
+            <Button type="submit" variant="contained">Login</Button>
           </div>
         </form>
       </div>
+     </div>
     </div>
   );
 };
